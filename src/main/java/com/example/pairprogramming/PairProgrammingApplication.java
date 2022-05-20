55a2a3166c1e966414556f9f029982b8b4a04e79
checkmyprojects <notnow.plz+github@gmail.com>
2022-05-20 11:10:05 +0200

    first

---
+++ src/main/java/com/example/pairprogramming/PairProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.pairprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairProgrammingApplication.class, args);
+    }
+
+}
